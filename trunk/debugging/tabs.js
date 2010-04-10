5ad4154bf2c85af8052c881e9000a3cdb715986f
anm9tr <anm9tr@c68170f6-2e29-2798-e11d-e89fca9e771e>
2010-04-10 18:50:27 +0000

    added comments to tabs.js

--- trunk/debugging/tabs.js
+++ trunk/debugging/tabs.js
@@ -1,3 +1,47 @@
+// Club AJAX General Purpose Code
+//
+// author:
+//		Mike Wilcox
+// site:
+//		http://clubajax.org
+// support:
+//		http://groups.google.com/group/clubajax
+//
+// clubajax.debugging.tabs
+//	DESCIPTION:
+//		Adds tabbing functionality to the console, whether that be Firebug
+//		or any other debugger that supports spaces (if HTML is used,
+//		multiple spaces become one).
+//	properties:
+//		console.tabLength: Number (defaults to 10)
+//			The amount of characters between tabs
+//		console.justify: String (defaults to left)
+//			Whether the tabs are right, left or center justified
+// methods:
+//		console.pad
+//			arguments: expects one, can be anything
+//			description:
+//				Adds spaces to the side(s) of the argument
+//				to space it out and simulate a tab-space.
+//
+//		console.tabs(thing, thing, [...])
+//			arguments: any number, all args can be anything
+//			description:
+//				Key function. Call console.tabs(arg, arg, arg)
+//				consecutively with presumably similar arguments.
+//
+// 	USAGE:
+// 		include file:
+//			<script src="clubajax/debugging/tabs.js"></script>
+//
+//	EXAMPLE:
+//		console.tabLength = 20;
+//		console.justify = "right"
+//		console.tabs("club", "ajax", "rocks");
+//		console.tabs("google", "code", "rocks");
+//		console.tabs("html5", "css3", "foobar!");
+//
+//
 (function(){
 	var initTabs = function(){
 		console.tabLength = 10;
@@ -17,5 +61,6 @@
 			this.log.apply(this, [msg]);
 		}
 	}
+	// we may need to check for IE8 here. Its console is hard to detect.
 	if(window.console){ initTabs(); }
 })();
