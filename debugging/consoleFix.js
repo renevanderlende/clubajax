b32585ff963f34a6f201da9b3313d233ef01f583
anm9tr <anm9tr@c68170f6-2e29-2798-e11d-e89fca9e771e>
2010-04-17 18:17:07 +0000

    update consoleFix test and JS comments

--- debugging/consoleFix.js
+++ debugging/consoleFix.js
@@ -12,21 +12,31 @@
 // clubajax.debugging.consoleFix
 //
 //	DESCRIPTION:
-//		Fixes some of the annoyances with the IE8 console.
+//		Allows the ability to turn the console on anf off. Use debug=true
+//		in a script before this loads, or in the page url, like:
+//			http://mypage/index.html?debug=true
+//		Without debug=true in one of these two places, the console is turned off
+//		to prevent throwing errors in users' browsers that do not have Firebug
+//		installed.
+//
+//		Also fixes some of the annoyances with the IE8 console:
 //			-	clears the logs on reload
 //			- 	adds spaces between logged arguments
+//			- 	adds stubs for Firebug commands
 //
 // 	USAGE:
 // 		include file:
 //			<script src="clubajax/debugging/consoleFix.js"></script>
 //
-//	No examples. Just include the script and the console will work better.
+//	Just include the script and the console will work better.
+// TESTS:
+//		See tests/consoleFix.html
 //
 var debug = window.debug || /debug=true/.test(document.location.href) || false;
 
 (function(){
-	var common = "info,debug,warn,error,log";
-	var more = "time,timeEnd,assert,count,trace,dir,dirxml,group,groupEnd";
+	var common = "info,warn,error,log";
+	var more = "debug,time,timeEnd,assert,count,trace,dir,dirxml,group,groupEnd,groupCollapsed,exception";
 	var fixIE = function(){
 		var calls = common.split(",");
 		for(var i=0;i<calls.length;i++){
@@ -44,8 +54,8 @@ var debug = window.debug || /debug=true/.test(document.location.href) || false;
 		// If closed it throws an error
 		try{ console.clear(); }catch(e){}
 	}
-	var hideConsole = function(){
-		var calls = (more+","+common).split(",");
+	var hideCalls = function(str){
+		var calls = str.split(",");
 		if(!window.console) console = {};
 		for(var i=0;i<calls.length;i++){
 			console[calls[i]] = function(){};
@@ -53,7 +63,8 @@ var debug = window.debug || /debug=true/.test(document.location.href) || false;
 	}
 	if(debug && /Trident/.test(window.navigator.userAgent)){
 		fixIE();
+		hideCalls(more);
 	}else if(!debug || !window.console){
-		hideConsole();
+		hideCalls(more+","+common);
 	}
 })();
