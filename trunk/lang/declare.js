99c1317093a61b667ec26c1248036f9f19307d68
anm9tr <anm9tr@c68170f6-2e29-2798-e11d-e89fca9e771e>
2011-06-12 20:40:25 +0000

    Added comments to declare inhertiance helper

--- trunk/lang/declare.js
+++ trunk/lang/declare.js
@@ -1,3 +1,54 @@
+// Club AJAX General Purpose Code
+//
+// Declare
+//
+// author:
+//		Mike Wilcox
+// site:
+//		http://clubajax.org
+// support:
+//		http://groups.google.com/group/clubajax
+//
+// clubajax.lang.declare
+//
+//	DESCRIPTION:
+//		Declare provides a simple inhertiance method for combining
+//		multiple objects into a single class.
+
+// 	USAGE:
+// 		include file:
+//			<script src="clubajax/lang/declare.js"></script>
+//
+//		var Foo = {
+//			Foo: function(){
+//				// initialize!
+//			},
+//			update: function(){
+//				// update something!
+//			}
+//		};
+//
+//		var Bar = {
+//			Bar: function(){
+//				// initialize!
+//			},
+//			draw: function(){
+//				// draw something!
+//			}
+//		};
+//
+//		var Zap = {
+//			Zap: function(){
+//				// initialize!
+//			},
+//			remove: function(){
+//				// remove something!
+//			}
+//		};
+//
+//		var CustomClass = declare(Foo, Bar, Zap);
+//
+//		
 declare = function(){
 	var i, a = arguments, constructors = [], subclasses = [], declaredClass;
 
